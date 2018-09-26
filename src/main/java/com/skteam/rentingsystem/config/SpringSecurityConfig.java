7a59b22d8180d55a10e0aeab2357b82c07119af3
Bartosz Suchocki <suchar1270@wp.pl>
2018-09-26 10:19:44 +0200

    Konfiguracja springa i hibernate. Podstawowy wygląd

---
+++ src/main/java/com/skteam/rentingsystem/config/SpringSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.skteam.rentingsystem.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// Na razie ten plik jest bardzo podstawowy i niewiele daje! Do dostosowanej
+	// autentykacji trzeba bêdzie go przebudowaæ
+
+	@Autowired
+	private DataSource securityDataSource;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/aa").authenticated().and().formLogin().loginPage("/showLoginForm")
+				.loginProcessingUrl("/authenticateTheUser").permitAll().and().logout().permitAll().and()
+				.exceptionHandling().accessDeniedPage("/accessDenied");
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(securityDataSource);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>Main Page</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link
+	href="${pageContext.request.contextPath}/resources/css/bootstrap.min.css"
+	rel="stylesheet">
+<link
+	href="${pageContext.request.contextPath}/resources/css/styles.min.css"
+	rel="stylesheet">
+<link
+	href="${pageContext.request.contextPath}/resources/css/custom-styles.css"
+	rel="stylesheet">
+</head>
+<body class="body">
+
+	<%@ include file="fragment/navigation.jspf"%>
+
+
+	<h2>This is the Main Page</h2>
+
+
+	<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script
+		src="${pageContext.request.contextPath}/resources/js/bootstrap.js"></script>
+</body>
+
+
+</html>
