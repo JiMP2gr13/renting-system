b292b77fdf5d116796fc00ea2e7c0a9d8f65bdd0
Bartosz Suchocki <suchar1270@wp.pl>
2018-09-28 12:02:14 +0200

    Dodanie adnotacji @Repository

--- src/main/java/com/skteam/rentingsystem/dao/ItemDAOImpl.java
+++ src/main/java/com/skteam/rentingsystem/dao/ItemDAOImpl.java
@@ -6,10 +6,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.skteam.rentingsystem.entity.Item;
 import com.skteam.rentingsystem.entity.User;
 
+@Repository
 public class ItemDAOImpl implements ItemDAO {
 
 	@Autowired
